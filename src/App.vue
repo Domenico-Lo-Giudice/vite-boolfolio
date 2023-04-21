db9c37902cc94a1d714a7c6f7c80c337a16641c4
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-21 18:07:20 +0200

    Print

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 
 export default {
   data() {
     return {
-      title: "Hello World",
+      title: "Hello",
+      projects: [],
     };
   },
 
   components: { AppHeader },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
 <template>
   <AppHeader />
   <h1>{{ title }}</h1>
+  <ul>
+    <li v-for="project in projects">{{ project.title }}</li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
