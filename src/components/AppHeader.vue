be01c3a7e56623b7ab9e6cf082c25d295ec9a44e
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-26 14:42:26 +0200

    Add router-link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,6 +12,22 @@ export default {
   <nav class="navbar bg-body-tertiary">
     <div class="container">
       <span class="navbar-brand mb-0 h1">{{ title }}</span>
+
+      <div>
+        <router-link
+          :to="{ name: 'home' }"
+          class="nav-link"
+        >
+          Homepage
+        </router-link>
+
+        <router-link
+          :to="{ name: 'contacts' }"
+          class="nav-link"
+        >
+          Contacts
+        </router-link>
+      </div>
     </div>
   </nav>
 </template>
