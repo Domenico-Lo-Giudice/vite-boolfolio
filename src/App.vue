66689c857bc74d527256d597cf86522925393665
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-26 14:48:34 +0200

    move container for Contacts & Homepage

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,9 @@ export default {
 
 <template>
   <AppHeader :title="title" />
-
-  <router-view></router-view>
+  <main class="container">
+    <router-view></router-view>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
